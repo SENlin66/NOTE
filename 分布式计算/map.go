f919b51d86b079b61104ac116635bf853cb3c6e7
SENlin66 <2357993918@qq.com>
2026-09-25 23:53:14 +0800

    Add files via upload

--- 分布式计算/map.go
+++ 分布式计算/map.go
@@ -1,1 +1,24 @@
+package main
 
+import (
+	"fmt"
+)
+
+func main() {
+	m := map[string]int{
+		"apple":  1,
+		"banana": 2,
+		"orange": 3,
+	}
+
+	n := map[string]int{
+		"apple":  1,
+		"banana": 2,
+		"pear":   3,
+	}
+	fmt.Println(m["apple"], n["banana"])
+	// for k,vm:=range m{
+	// 	if
+	// }
+
+}
